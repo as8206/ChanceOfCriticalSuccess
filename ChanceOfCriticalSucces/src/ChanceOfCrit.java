3f9e591c4afca14abf6de7bc21e1babcea4f9d39
as8206 <as8206@mct164l07>
2018-02-26 10:27:28 -0500

    initial

---
+++ ChanceOfCriticalSucces/src/ChanceOfCrit.java
@@ -0,0 +1,34 @@
+
+public class ChanceOfCrit 
+{
+	public static final int TRIALS = 20;
+	public static final int RUN_TIMES = 1000;
+	public static final int MAX_TRIALS = 30000;
+	
+	public int BinomialArray[] = new int[MAX_TRIALS];
+	public int GeometricArray[] = new int[MAX_TRIALS];
+	public int NegitiveGeometricArray[] = new int [MAX_TRIALS];
+	
+	public static void main(String[] args)
+	{
+
+	}
+	
+	public int RollD20()
+	{
+		return 0;
+	}
+	
+	public void makeBinomialGraph()
+	{
+		
+	}
+	public void makeGeometricGraph()
+	{
+		
+	}
+	public void makeNegitiveGeometricGraph()
+	{
+		
+	}
+}
